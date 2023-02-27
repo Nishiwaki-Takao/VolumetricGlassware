8da11b13f6f43dcae0330ce36691a438429658b7
nishi <nisiwaki@shinwa-made.co.jp>
2023-02-27 22:25:16 +0900

    メスフラスコのJIS R 3505のすべてを記録した。

--- VolumetricGlassware/Common/AbstractGlassware.py
+++ VolumetricGlassware/Common/AbstractGlassware.py
@@ -1,5 +1,23 @@
-class my_class(object):
-    pass
+from telnetlib import SE
+from uncertainties import ufloat
+from abc import ABC
+import Enums
+
+
+class VolumetricGlassWare(ABC):
+    def __init__(self,
+                 Capacity: float,
+                 Tolerance: float,
+                 Class: str,
+                 Calibration: str):
+        self._Capacity = Capacity
+        self._Tolerance = Tolerance
+        self._Class = Enums.ToleranceClass[Class]
+        self._Calibration = Enums.Calibration[Calibration]
+        self.
+
+
+
 
 
 
